039302efba8e5220cc93588697ea16809eba1c0e
Sanchi Chakraborty <sachakr@microsoft.com>
2021-09-18 14:08:16 +0530

    NestJs based Microservice with MongoDb demo

---
+++ src/blog/blog.module.ts
@@ -0,0 +1,24 @@
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { BlogService } from './blog.service';
+import { BlogController } from './blog.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogSchema } from './schemas/blog.schema';
+import { NestModule } from '@nestjs/common/interfaces/modules';
+import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Post', schema: BlogSchema }]),
+  ],
+  providers: [BlogService],
+  controllers: [BlogController]
+})
+export class BlogModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+    consumer.apply(AuthenticationMiddleware).forRoutes(
+      { method: RequestMethod.POST, path: '/blog/post' },
+      { method: RequestMethod.PUT, path: '/blog/edit' },
+      { method: RequestMethod.DELETE, path: '/blog/delete' }
+    )
+  }
+}
